4771f64b8bba443418cb4903f233603a83638564
jj <jordanjhubbard@gmail.com>
2024-06-20 11:21:13 -0700

    No changes

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,8 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  #chaotic.scx.enable = true;
+  #chaotic.scx.scheduler = "scx_lavd";
   boot.kernelParams = [ "nvidia-drm.modeset=1" "nvidia-drm.fbdev=1" ];
 
   nix.gc = {
