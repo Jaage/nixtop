759881a9afd3a5dff40d2b29e9bcd22cbf5b934b
Jaage <jordanjhubbard@gmail.com>
2025-05-09 14:54:11 -0700

    No changes

--- configuration.nix
+++ configuration.nix
@@ -232,11 +232,11 @@ in
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedUDPPorts = [ 22 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
