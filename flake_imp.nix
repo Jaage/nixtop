d726d00648ed9d977583aabfa583a09ecdad43fa
Jaage <69830700+Jaage@users.noreply.github.com>
2025-06-17 08:09:01 -0700

    Create flake_imp.nix

---
+++ flake_imp.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    impermanence.url = "github:nix-community/impermanence";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          inputs.impermanence.nixosModules.impermanence
+        ];
+      };
+    };
+}
